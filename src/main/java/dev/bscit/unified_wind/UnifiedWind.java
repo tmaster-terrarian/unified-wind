c63610fdaf805fe2fc09ac89d63dc3f6d071cff6
bscit <bscitdv@gmail.com>
2026-05-17 22:21:31 -0700

    no more warnings!!!

--- src/main/java/dev/bscit/unified_wind/UnifiedWind.java
+++ src/main/java/dev/bscit/unified_wind/UnifiedWind.java
@@ -224,7 +224,7 @@ public class UnifiedWind
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    @EventBusSubscriber(modid = MODID, value = Dist.CLIENT)
     public static class ClientModEvents
     {
         @SubscribeEvent
