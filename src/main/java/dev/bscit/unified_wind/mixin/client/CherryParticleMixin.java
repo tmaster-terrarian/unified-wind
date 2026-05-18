c2ae3a3f54596aa2add9a977e79003a13c6cb843
bscit <bscitdv@gmail.com>
2026-05-17 19:35:03 -0700

    improve CherryParticleMixin

--- src/main/java/dev/bscit/unified_wind/mixin/client/CherryParticleMixin.java
+++ src/main/java/dev/bscit/unified_wind/mixin/client/CherryParticleMixin.java
@@ -2,8 +2,9 @@ package dev.bscit.unified_wind.mixin.client;
 
 import dev.bscit.unified_wind.CommonConfig;
 import dev.bscit.unified_wind.UnifiedWind;
-import dev.bscit.unified_wind.mixin.client.accessor.ParticleAccessor;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.CherryParticle;
+import net.minecraft.client.particle.TextureSheetParticle;
 import org.joml.Vector3f;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(CherryParticle.class)
-public class CherryParticleMixin
+public abstract class CherryParticleMixin extends TextureSheetParticle
 {
     @Shadow
     private float rotSpeed;
@@ -21,44 +22,40 @@ public class CherryParticleMixin
     @Shadow @Final
     private float spinAcceleration;
 
+    protected CherryParticleMixin(ClientLevel level, double x, double y, double z)
+    {
+        super(level, x, y, z);
+    }
+
     @Inject(method = "tick", at = @At(value = "HEAD"), cancellable = true)
     private void unifiedWind$injectTick(CallbackInfo ci)
     {
         if(!CommonConfig.get().compat.vanilla)
             return;
-        var p = (CherryParticle)(Object)this;
-        var accessor = (ParticleAccessor)this;
-
-        accessor.unifiedWind$setXo(accessor.unifiedWind$getX());
-        accessor.unifiedWind$setYo(accessor.unifiedWind$getY());
-        accessor.unifiedWind$setZo(accessor.unifiedWind$getZ());
-
-        p.setLifetime(p.getLifetime() - 1);
-        if(p.getLifetime() <= 0)
-            p.remove();
-
-        if(!p.isAlive())
+        xo = x;
+        yo = y;
+        zo = z;
+        if(this.lifetime-- <= 0)
+            remove();
+        if(removed)
             return;
-
-        float f = 300 - p.getLifetime();
+        float f = 300 - lifetime;
         float f1 = Math.min(f / 300, 1);
-        Vector3f wind = UnifiedWind.getWind(p.getPos().x, p.getPos().y, p.getPos().z);
+        Vector3f wind = UnifiedWind.getWind(x, y, z);
         double d0 = wind.x * Math.pow(f1, 1.25) * 0.008;
         double d1 = wind.z * Math.pow(f1, 1.25) * 0.008;
-        accessor.unifiedWind$setXd(accessor.unifiedWind$getXd() + d0);
-        accessor.unifiedWind$setZd(accessor.unifiedWind$getZd() + d1);
-        accessor.unifiedWind$setYd(accessor.unifiedWind$getYd() - accessor.unifiedWind$getGravity());
-        this.rotSpeed += this.spinAcceleration / 20;
-        accessor.unifiedWind$setORoll(accessor.unifiedWind$getRoll());
-        accessor.unifiedWind$setRoll(accessor.unifiedWind$getRoll() + this.rotSpeed / 20);
-        p.move(accessor.unifiedWind$getXd(), accessor.unifiedWind$getYd(), accessor.unifiedWind$getZd());
-        if(accessor.unifiedWind$getOnGround() || p.getLifetime() < 299 && (accessor.unifiedWind$getXd() == 0 || accessor.unifiedWind$getZd() == 0))
-            p.remove();
-
-        accessor.unifiedWind$setXd(accessor.unifiedWind$getXd() * accessor.unifiedWind$getFriction());
-        accessor.unifiedWind$setYd(accessor.unifiedWind$getYd() * accessor.unifiedWind$getFriction());
-        accessor.unifiedWind$setZd(accessor.unifiedWind$getZd() * accessor.unifiedWind$getFriction());
-
+        xd += d0;
+        zd += d1;
+        yd -= gravity;
+        rotSpeed += spinAcceleration / 20;
+        oRoll = roll;
+        roll += rotSpeed / 20;
+        move(xd, yd, zd);
+        if(onGround || lifetime < 299 && (xd == 0 || zd == 0))
+            remove();
+        xd *= friction;
+        yd *= friction;
+        zd *= friction;
         ci.cancel();
     }
 }
